8d333089ea8adc7eab2ee2b88ce748ce585b17ea
YakovMashin <yakovmashin2004@gmail.com>
2022-03-02 09:26:11 +0200

    Changing Icon
    ву

--- lib/main.dart
+++ lib/main.dart
@@ -108,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.security),
+        child: const Icon(Icons.ac_unit_rounded),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
